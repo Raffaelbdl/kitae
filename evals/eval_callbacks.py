5419cb9398837a8e4f5b8431e945c7b6da393e93
Raffaelbdl <raffaelbdl@gmail.com>
2023-12-01 18:49:57 +0900

    ⚡ Update evals

--- evals/eval_callbacks.py
+++ evals/eval_callbacks.py
@@ -1,14 +1,26 @@
+from abc import ABC, abstractmethod
 import os
+from collections import deque
 from datetime import datetime
 from time import time
 
 from rl.callback import Callback, CallbackData
 
 
-class TimeCallback(Callback):
+class EvalCallback(Callback, ABC):
     def __init__(self, task: str, n_envs: int) -> None:
+        Callback.__init__(self)
         self.task = task
         self.n_envs = n_envs
+
+    @abstractmethod
+    def write_results(self, evaluated: str):
+        pass
+
+
+class TimeCallback(EvalCallback):
+    def __init__(self, task: str, n_envs: int) -> None:
+        EvalCallback.__init__(self, task, n_envs)
 
         self.update_duration_list = []
         self.start_train_time = 0.0
@@ -21,6 +33,7 @@ class TimeCallback(Callback):
         filepath = os.path.join(
             os.path.dirname(__file__),
             evaluated,
+            "TimeCallback",
             datetime.now().strftime("%Y_%m_%d_%H_%M"),
         )
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
@@ -30,7 +43,8 @@ class TimeCallback(Callback):
                     "TimeCallback Outputs >>>>> \n",
                     f"Task : {self.task} | n_envs : {self.n_envs} \n",
                     f"Training Duration : {self.training_duration:.1f} \n",
-                    f"Mean Update Duration : {self.update_duration:.1f} \n",
+                    f"Mean Update Duration : {self.update_duration:.3f} \n",
+                    f"Cumulate Update Duration : {sum(self.update_duration_list)} \n",
                 ]
             )
 
@@ -51,3 +65,37 @@ class TimeCallback(Callback):
 
         print("Training duration : ", self.training_duration)
         print("Mean update duration : ", self.update_duration)
+
+
+class ScoreCallback(EvalCallback):
+    def __init__(self, task: str, n_envs: int, maxlen: int) -> None:
+        EvalCallback.__init__(self, task, n_envs)
+
+        self.episode_returns = deque(maxlen=maxlen)
+        self.mean_episode_return = 0.0
+
+    def write_results(self, evaluated: str):
+        filepath = os.path.join(
+            os.path.dirname(__file__),
+            evaluated,
+            "ScoreCallback",
+            datetime.now().strftime("%Y_%m_%d_%H_%M"),
+        )
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, "w") as f:
+            f.writelines(
+                [
+                    "ScoreCallback Outputs >>>>> \n",
+                    f"Task : {self.task} | n_envs : {self.n_envs} \n",
+                    f"Mean Score for last {len(self.episode_returns)} : {self.mean_episode_return}",
+                ]
+            )
+
+    def on_episode_end(self, callback_data: CallbackData):
+        self.episode_returns.append(callback_data.episode_return)
+
+    def on_train_end(self, callback_data: CallbackData):
+        self.mean_episode_return = sum(self.episode_returns)
+        self.mean_episode_return /= len(self.episode_returns)
+
+        print("Mean sore : ", self.mean_episode_return)
