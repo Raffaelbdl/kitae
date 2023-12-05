ad8cfd81f1b7dcabab43d9d108f04a4779bec489
Raffaelbdl <raffaelbdl@gmail.com>
2023-12-05 13:26:25 +0900

    ✨Add epreturn callback

---
+++ rl/callbacks/episode_return_callback.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+from rl.callbacks.callback import Callback, CallbackData
+
+
+class EpisodeReturnCallback(Callback):
+    def __init__(self, *, population_size: int = 1, buffer_length: int = 20) -> None:
+        super().__init__()
+
+        self.population_size = population_size
+        self.episode_return_buffer = [
+            deque(maxlen=buffer_length) for _ in range(population_size)
+        ]
+
+    def on_episode_end(self, callback_data: CallbackData) -> None:
+        data = callback_data.episode_end_data
+        self.episode_return_buffer[data.agent_id].append(data.episode_return)
+
+    def get_logs(self) -> dict:
+        logs = {}
+        for i in range(len(self.episode_return_buffer)):
+            buffer = self.episode_return_buffer[i]
+            if len(buffer) > 0:
+                if self.population_size > 1:
+                    logs[f"agent_{i}/mean_episode_return"] = sum(buffer) / len(buffer)
+                else:
+                    logs["mean_episode_return"] = sum(buffer) / len(buffer)
+        return logs
