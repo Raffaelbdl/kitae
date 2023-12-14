85f68323eda35223a85a454fa118c2c5354bae39
Raffaelbdl <raffaelbdl@gmail.com>
2023-12-14 18:23:47 +0900

    ✨ Add classmethod to load config + taskname

--- rl/config.py
+++ rl/config.py
@@ -2,6 +2,7 @@ from dataclasses import asdict, dataclass
 
 from gymnasium.spaces import Space
 from ml_collections import ConfigDict
+import yaml
 
 
 @dataclass
@@ -28,6 +29,7 @@ class TrainConfig:
 
 @dataclass
 class EnvConfig:
+    task_name: str
     observation_space: Space
     action_space: Space
     n_envs: int
@@ -65,5 +67,45 @@ class AlgoConfig(ConfigDict):
             "env_cfg": asdict(env_config),
         }
         ConfigDict.__init__(self, config)
+
+        # env_cfg cannot be serialized to yaml because it holds gymnasium spaces.
+        # The chosen solution is to transfer the meaningful information for the user.
+        self.task_name = self.env_cfg.task_name
         self.train_cfg.n_envs = self.env_cfg.n_envs
         self.train_cfg.n_agents = self.env_cfg.n_agents
+
+    @classmethod
+    def create_config_from_file(
+        cls, config_path: str, env_config: EnvConfig
+    ) -> ConfigDict:
+        """Creates an AlgoConfig instance from a yaml file.
+
+        Technically, the created instance is not from the AlgoConfig class. But
+        it does behave exactly the same.
+
+        Args:
+            config_path: A string path to the yaml configuration file.
+            env_config: An instance of EnvConfig.
+
+        Returns:
+            An instance of ConfigDict that behaves similarly to AlgoConfig.
+
+        Raises:
+            KeyError: If one key is missing from the yaml config file.
+        """
+        with open(config_path, "r") as file:
+            config_dict = yaml.load(file, yaml.SafeLoader)
+
+        for key in ["seed", "algo_params", "update_cfg", "train_cfg"]:
+            if key not in config_dict.keys():
+                raise KeyError(
+                    f"KeyError : {key} key is not present in the config file"
+                )
+
+        config = ConfigDict(config_dict | {"env_cfg": asdict(env_config)})
+
+        config.task_name = config.env_cfg.task_name
+        config.train_cfg.n_envs = config.env_cfg.n_envs
+        config.train_cfg.n_agents = config.env_cfg.n_agents
+
+        return config
