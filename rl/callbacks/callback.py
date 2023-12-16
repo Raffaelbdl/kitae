53719c2893e77c2bcbc9a20c451eff1494aed0e2
Raffaelbdl <raffaelbdl@gmail.com>
2023-12-16 10:45:20 +0900

    📚 Add docstring for callbacks

--- rl/callbacks/callback.py
+++ rl/callbacks/callback.py
@@ -1,40 +1,51 @@
+"""Contains the base classes for callbacks."""
+
 from dataclasses import dataclass, field
 
 
 @dataclass
 class OnEpisodeEndData:
-    agent_id: int = field(default=0)
-    episode_return: float = field(default=0.0)
+    agent_id: int = 0
+    episode_return: float = 0
 
 
 @dataclass
 class CallbackData:
     logs: dict = field(default_factory=lambda: {})
+    episode_end_data: OnEpisodeEndData = field(
+        default_factory=lambda: OnEpisodeEndData()
+    )
 
-    episode_end_data: OnEpisodeEndData = field(default=OnEpisodeEndData())
-
-    @staticmethod
-    def on_episode_end(agent_id: int, episode_return: float):
-        return CallbackData(episode_end_data=OnEpisodeEndData(agent_id, episode_return))
+    @classmethod
+    def on_episode_end(cls, agent_id: int, episode_return: float):
+        return cls(episode_end_data=OnEpisodeEndData(agent_id, episode_return))
 
 
 class Callback:
+    """Base class for callbacks."""
+
     def on_train_start(self, callback_data: CallbackData):
+        """Called when the training starts."""
         pass
 
     def on_train_end(self, callback_data: CallbackData):
+        """Called when the training ends."""
         pass
 
     def on_episode_end(self, callback_data: CallbackData):
+        """Called when any environment is terminated."""
         pass
 
     def on_update_start(self, callback_data: CallbackData):
+        """Called when the update starts."""
         pass
 
     def on_update_end(self, callback_data: CallbackData):
+        """Called when the update ends."""
         pass
 
     def get_logs(self) -> dict:
+        """Returns information to log."""
         return {}
 
 
