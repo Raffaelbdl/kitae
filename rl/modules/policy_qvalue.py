33603fe7bf9f5280fbcdab9e995c7b713cfe3242
Raffaelbdl <raffaelbdl@gmail.com>
2024-01-29 10:30:45 +0900

    Add asc

--- rl/modules/policy_qvalue.py
+++ rl/modules/policy_qvalue.py
@@ -19,3 +19,23 @@ class TrainStatePolicyQValue:
 
     policy_state: TrainState
     qvalue_state: TrainState
+
+
+@chex.dataclass
+class TrainStatePolicyQValueTemperature:
+    """Jittable collection of policy and qvalue TrainState.
+
+    Contrary to on-policy algorithms, off-policy algorithms often require
+    both separate encoders and separate updates of policy and qvalue modules.
+    This class store them separately, in a chex.dataclass so that it can be
+    used in jitted function.
+
+    Attributes:
+        policy_state, qvalue_state:
+            Custom TrainState that hold a `target_params` field.
+        temperature_state: TrainState
+    """
+
+    policy_state: TrainState
+    qvalue_state: TrainState
+    temperature_state: TrainState
