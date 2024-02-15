7d7dcacd383eb4dabec04855e258f0a7c6fb2dec
Raffaelbdl <raffaelbdl@gmail.com>
2024-02-15 18:02:59 +0900

    Create pipeline module class

---
+++ rl/algos/pipelines/__init__.py
@@ -0,0 +1,15 @@
+from rl.algos.pipelines.experience_pipeline import ExperienceTransform
+from rl.algos.pipelines.update_pipeline import UpdateModule
+
+
+class PipelineModule(ExperienceTransform, UpdateModule):
+    @property
+    def experience_transform(self) -> ExperienceTransform:
+        return ExperienceTransform(
+            process_experience_fn=self.process_experience_fn, state=self.state
+        )
+
+    @property
+    def update_module(self) -> UpdateModule:
+        return UpdateModule(update_fn=self.update_fn, state=self.state)
+
