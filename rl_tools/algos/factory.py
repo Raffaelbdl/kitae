8f39c9b3f9f9c91303e61431fa2612b794f082e0
Raffaelbdl <raffaelbdl@gmail.com>
2024-02-27 18:50:22 +0900

    Add comments and rm old function

--- rl_tools/algos/factory.py
+++ rl_tools/algos/factory.py
@@ -25,26 +25,44 @@ Factory = Callable[..., Callable]
 
 
 def fn_parallel(fn: Callable) -> Callable:
-    """args must be entered in the same order as in fn to allow vmapping"""
+    """Parallelizes a function for mutliple agents.
 
-    def _fn(params: Params, *trees, **hyperparams):
-        res = jax.tree_util.tree_map(
-            functools.partial(fn, params, **hyperparams), *trees
+    Typical usage for function with args:
+        - state
+        - trees of structure: {"agent_0": Array, "agent_1": Array, ...}
+        - hyperparameters
+
+    The wrapped function returns a list of trees with the same structure as input.
+
+    Warning: args must be entered in the same order as in fn to allow vmapping.
+    """
+
+    def wrapped(state: Any, *trees, **hyperparams):
+        results = jax.tree_util.tree_map(
+            functools.partial(fn, state, **hyperparams), *trees
         )
 
-        n_outputs = len(list(res.values())[0])
+        # transform the structure of results
+        # output = {"agent_0": [out1, out2], "agent_1": [out1, out2]}
+        # -> [{"agent_1": out1, "agent_2": out1}, {"agent_1": out2, "agent_2": out2}]
+
+        n_outputs = len(list(results.values())[0])
         outputs = [{} for _ in range(n_outputs)]
-        for a, v in res.items():
-            for i in range(len(outputs)):
-                outputs[i][a] = v[i]
+
+        for key, value in results.items():
+            for out_pos in range(n_outputs):
+                outputs[out_pos][key] = value[out_pos]
+
         return outputs
 
-    return _fn
+    return wrapped
 
 
 def explore_general_factory(
     explore_fn: Callable, vectorized: bool, parallel: bool
 ) -> Callable:
+    """Generalizes a explore_fn to vector and parallel envs."""
+
     def input_fn(inputs):
         if not vectorized:
             return jax.tree_map(lambda x: jnp.expand_dims(x, axis=0), inputs)
@@ -57,78 +75,13 @@ def explore_general_factory(
             return jax.tree_map(lambda x: jnp.squeeze(x, axis=0), outputs)
         return outputs
 
-    def fn(state: Any, key: jax.Array, *trees, **hyperparams):
+    def general_fn(state: Any, key: jax.Array, *trees, **hyperparams):
         inputs = input_fn(trees)
         results = explore_fn(state, key, *inputs, **hyperparams)
         outputs = output_fn(results)
         return outputs
 
-    return jax.jit(fn)
-
-
-def process_experience_general_factory(
-    process_experience_fn: Callable,
-    vectorized: bool,
-    parallel: bool,
-    experience_type: NamedTuple = Experience,
-):
-    def fn(state: TrainState, key: jax.Array, experience: experience_type):
-        experience = stack_experiences(experience)
-
-        def _process_experience_fn(key, *experience):
-            return process_experience_fn(state, key, experience_type(*experience))
-
-        if parallel and vectorized:
-            keys = {}
-            for agent, obs in experience.observation.items():
-                new_keys = jax.random.split(key, obs.shape[1] + 1)
-                # swap axes for vmapping
-                key, keys[agent] = new_keys[0], new_keys[1:].swapaxes(0, 1)
-
-            # TODO make a test for this section
-            # especially the part where a new tuple of length 1 is created
-            outputs = jax.tree_map(
-                jax.vmap(_process_experience_fn, in_axes=1, out_axes=1),
-                keys,
-                *experience,
-            )
-
-            def concat_and_reshape(*x):
-                out = jnp.concatenate(x, axis=1)
-                return jnp.reshape(out, (-1, *out.shape[2:]))
-
-            return jax.tree_map(concat_and_reshape, *zip(outputs.values()))[0]
-
-        elif parallel:
-            keys = {}
-            for agent in experience.observation.keys():
-                key, _k = jax.random.split(key, 2)
-                keys[agent] = _k
-
-            # TODO make a test for this section
-            # especially the part where a new tuple of length 1 is created
-            outputs = jax.tree_map(_process_experience_fn, keys, *experience)
-
-            def stack_and_reshape(*x):
-                out = jnp.stack(x, axis=1)
-                return jnp.reshape(out, (-1, *out.shape[2:]))
-
-            return jax.tree_map(stack_and_reshape, *zip(outputs.values()))[0]
-
-        elif vectorized:
-            keys = jax.random.split(key, experience.observation.shape[1]).swapaxes(0, 1)
-            outputs = jax.vmap(_process_experience_fn, in_axes=1, out_axes=1)(
-                keys, *experience
-            )
-            outputs = jax.tree_map(
-                lambda x: jnp.reshape(x, (-1, *x.shape[2:])), outputs
-            )
-            return outputs
-
-        else:
-            return process_experience_fn(state, key, experience)
-
-    return jax.jit(fn)
+    return general_fn
 
 
 class AlgoFactory:
