7343c588e9f4c1e822c71b5ccf08de79c5b98858
Raffaelbdl <raffaelbdl@gmail.com>
2024-03-01 14:45:59 +0900

    Update tests

--- tests/unit/envs/test_make.py
+++ tests/unit/envs/test_make.py
@@ -9,29 +9,28 @@ from rl_tools.envs.make import make_vec_env
 
 def test_make_env():
     env = make_env("Dummy", 0, capture_video=False, run_name="")()
-    assert isinstance(env, gym.wrappers.RecordEpisodeStatistics)
     with pytest.raises(AttributeError):
         env.get_wrapper_attr("recording")
 
     env = make_env("Dummy", 0, capture_video=True, run_name="")()
-    assert isinstance(env, gym.wrappers.RecordEpisodeStatistics)
     env.get_wrapper_attr("recording")  # won't raise error
 
 
 def test_make_single_env():
     env = make_single_env("Dummy", capture_video=False, run_name="")
-    assert isinstance(env, gym.vector.SyncVectorEnv)
+    assert env.get_wrapper_attr("num_envs") == 1
+    assert isinstance(env, gym.wrappers.RecordEpisodeStatistics)
 
 
 def test_wrap_single_env():
     env = gym.make("Dummy")
     env = wrap_single_env(env)
-    assert isinstance(env, gym.vector.SyncVectorEnv)
-    assert isinstance(env.envs[0], gym.wrappers.RecordEpisodeStatistics)
+    assert env.get_wrapper_attr("num_envs") == 1
+    assert isinstance(env, gym.wrappers.RecordEpisodeStatistics)
 
 
 def test_vec_env():
     n_envs = 6
     env = make_vec_env("Dummy", n_envs, capture_video=False, run_name="")
-    assert isinstance(env, gym.vector.AsyncVectorEnv)
-    assert env.num_envs == n_envs
+    assert env.get_wrapper_attr("num_envs") == 6
+    assert isinstance(env, gym.wrappers.RecordEpisodeStatistics)
