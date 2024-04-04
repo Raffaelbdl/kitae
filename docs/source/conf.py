3761eaff93dd77cc706a4f3134a9259ebc0f8d0d
Raffaelbdl <raffaelbdl@gmail.com>
2024-04-04 13:30:32 +0900

    Increase release number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "Kitae"
 copyright = "2024, RaffaelBDL"
 author = "RaffaelBDL"
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
