6d4609cab5b234c2f07027a12b056ee538769634
Raffaelbdl <raffaelbdl@gmail.com>
2024-04-04 13:01:54 +0900

    Add save docstring

--- kitae/save.py
+++ kitae/save.py
@@ -15,6 +15,7 @@ from kitae.config import dump_algo_config
 
 
 def default_run_name(env_id: str) -> str:
+    """Generates a default name for a run."""
     return f"{env_id}/{env_id}__{int(time.time())}"
 
 
@@ -22,14 +23,18 @@ class Saver:
     """Saver class for agents.
 
     Handles saving during training and restoring from checkpoints.
+
+    Args:
+        dir (Path): The path where the agent is saved.
+        writer (SummaryWriter): A SummaryWriter to log metrics.
     """
 
-    def __init__(self, dir: str | Path, base: IAgent) -> None:
+    def __init__(self, dir: str | Path, agent: IAgent) -> None:
         """Initializes a Saver instance for an agent.
 
         Args:
             dir: A string or path-like path to the saving directory.
-            base: The parent agent.
+            agent: The parent agent.
         """
         self.dir = dir = Path(dir)
 
@@ -41,28 +46,48 @@ class Saver:
             dir, self.ckptr, self.options
         )
 
-        self.save_base_data(dir, base)
+        self.save_base_data(dir, agent)
 
         self.writer = SummaryWriter(dir.joinpath("logs"))
 
-    def save_base_data(self, dir_path: Path, base: IAgent) -> None:
+    def save_base_data(self, dir_path: Path, agent: IAgent) -> None:
+        """Saves the original information.
+
+        Args:
+            dir_path (Path): The path where the agent must be saved.
+            agent (IAgent): The agent whose information must be saved.
+        """
         config_dir = dir_path.joinpath("config")
-        dump_algo_config(base.config, config_dir)
+        dump_algo_config(agent.config, config_dir)
 
         extra_info = {
-            "run_name": base.run_name,
-            "preprocess_fn": base.preprocess_fn,
+            "run_name": agent.run_name,
+            "preprocess_fn": agent.preprocess_fn,
         }
         with config_dir.joinpath("extra").open("wb") as f:
             cloudpickle.dump(extra_info, f)
 
-    def save(self, step: int, ckpt: dict[str, Any]):
+    def save(self, step: int, ckpt: dict[str, Any]) -> None:
+        """Saves a checkpoint.
+
+        Args:
+            step (int): The current step of the environment.
+            ckpt (dict): A checkpoint to save.
+        """
         save_args = orbax_utils.save_args_from_target(ckpt)
         self.ckpt_manager.save(step, ckpt, save_kwargs={"save_args": save_args})
 
     def restore_latest_step(
         self, base_state_dict: dict[str, Any]
     ) -> tuple[int, dict[str, Any]]:
+        """Restores the state of an agent from the latest step.
+
+        Args:
+            base_state_dict (dict): The state of the agent before restoring it.
+
+        Returns:
+            The latest step as an int and the restored state of the agent.
+        """
         step = self.ckpt_manager.latest_step()
         return (
             step,
@@ -71,7 +96,28 @@ class Saver:
 
 
 class SaverContext(AbstractContextManager):
+    """A context to ensures that the agent state is saved when the training is interrupted.
+
+    Tip:
+        Typical usage::
+
+            with SaverContext(saver, save_frequency) as s:
+                for step in range(n_env_steps):
+                    ...
+
+                    s.update(step, agent.state)
+
+    Attributes:
+        saver (Saver): A saver instance.
+    """
+
     def __init__(self, saver: Saver, save_frequency: int) -> None:
+        """Initializes a SaverContext instance.
+
+        Args:
+            saver (Saver): A Saver instance.
+            save_frequency (int): The frequency at which the agent's state must be saved.
+        """
         super().__init__()
         self.saver = saver
 
@@ -79,7 +125,13 @@ class SaverContext(AbstractContextManager):
         self.cur_step = 0
         self.cur_state = None
 
-    def update(self, step: int, state: train_state.TrainState):
+    def update(self, step: int, state: train_state.TrainState) -> None:
+        """Informs the Saver of a new state, and saves it when necessary.
+
+        Args:
+            step (int): The current step of the environment.
+            state (TrainState): The state of the agent.
+        """
         self.cur_step = step
         self.cur_state = state
 
@@ -91,12 +143,8 @@ class SaverContext(AbstractContextManager):
 
         self.saver.save(step, state)
 
-    def __exit__(
-        self,
-        __exc_type: type[BaseException] | None,
-        __exc_value: BaseException | None,
-        __traceback: TracebackType | None,
-    ) -> bool | None:
+    def __exit__(self, *args, **kwargs) -> bool | None:
+        """Saves the agent's state when the context is exited."""
         if self.cur_state is None:
             return
 
