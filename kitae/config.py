2501e0ba1a7b5d82535010d9e84f744c8c6ea826
Raffaelbdl <raffaelbdl@gmail.com>
2024-06-03 11:27:56 +0900

    Rm previous config load and dump fns

--- kitae/config.py
+++ kitae/config.py
@@ -1,7 +1,5 @@
 from dataclasses import asdict, dataclass
-import os
 from pathlib import Path
-
 
 import cloudpickle
 from gymnasium.spaces import Space
@@ -47,61 +45,7 @@ class AlgoConfig:
     env_cfg: EnvConfig
 
 
-def dump_algo_config(config: AlgoConfig, dir_path: str | Path) -> None:
-    """Dumps an AlgoConfig in the given directory.
-
-    Args:
-        config (AlgoConfig): An agent's config.
-        dir_path (str | Path): The path where the config must be saved.
-    """
-    dir_path = Path(dir_path)
-    os.makedirs(dir_path, exist_ok=True)
-
-    env_cfg = config.env_cfg
-
-    # save custom instance type for yaml-dumping
-    algo_params_type = type(config.algo_params)
-    with dir_path.joinpath("algo_params_type").open("wb") as f:
-        cloudpickle.dump(algo_params_type, f)
-
-    algo_config_dict = asdict(config)
-    algo_config_dict.pop("env_cfg")  # rm because cannot be yaml-dumped
-
-    with dir_path.joinpath("algo_config.yaml").open("w") as f:
-        yaml.dump(algo_config_dict, f)
-    with dir_path.joinpath("env_config").open("wb") as f:
-        cloudpickle.dump(env_cfg, f)
-
-
-def load_algo_config(dir_path: str | Path) -> AlgoConfig:
-    """Loads an AlgoConfig from the given directory.
-
-    Args:
-        dir_path (str | Path): The path where the config is saved.
-
-    Returns:
-        The AlgoConfig saved at the given location.
-    """
-    dir_path = Path(dir_path)
-
-    with dir_path.joinpath("algo_params_type").open("rb") as f:
-        algo_params_type = cloudpickle.load(f)
-
-    with dir_path.joinpath("algo_config.yaml").open("r") as f:
-        algo_config_dict = yaml.load(f, yaml.SafeLoader)
-    with dir_path.joinpath("env_config").open("rb") as f:
-        algo_config_dict["env_cfg"] = cloudpickle.load(f)
-
-    return AlgoConfig(
-        seed=algo_config_dict["seed"],
-        algo_params=algo_params_type(**algo_config_dict["algo_params"]),
-        update_cfg=UpdateConfig(**algo_config_dict["update_cfg"]),
-        train_cfg=TrainConfig(**algo_config_dict["train_cfg"]),
-        env_cfg=algo_config_dict["env_cfg"],
-    )
-
-
-from save.serializable import SerializableObject, SerializableDict, Serializable
+from save.serializable import Serializable
 
 
 class ConfigSerializable(Serializable):
