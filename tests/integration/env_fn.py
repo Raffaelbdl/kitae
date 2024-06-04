731d8213b305eab41807e483511a2e6210632a71
Raffaelbdl <raffaelbdl@gmail.com>
2024-06-04 17:44:50 +0900

    Rm ss

--- tests/integration/env_fn.py
+++ tests/integration/env_fn.py
@@ -1,10 +1,6 @@
 from typing import Any, SupportsFloat
-import envpool
 import gymnasium as gym
 from gymnasium.spaces import Discrete, Box
-from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
-import supersuit as ss
-from kitae.envs.wrappers.compatibility import EnvpoolCompatibility
 from kitae.envs.make import wrap_single_env
 
 
@@ -32,25 +28,3 @@ class ContinuousEnv(RandomEnv):
 
 def make_continuous_env():
     return wrap_single_env(ContinuousEnv())
-
-
-def create_atari() -> gym.Env:
-    env = gym.make("ALE/Pong-v5", frameskip=1)
-    env = AtariPreprocessing(env, grayscale_newaxis=False)
-    env = ss.frame_stack_v2(env, 4, -1)
-    return env
-
-
-def create_cartpole() -> gym.Env:
-    env = gym.make("CartPole-v1")
-    return env
-
-
-def create_atari_envpool(n_envs: int):
-    env = envpool.make("Pong-v5", env_type="gymnasium", num_envs=n_envs, seed=0)
-    return EnvpoolCompatibility(env)
-
-
-def create_cartpole_envpool(n_envs: int):
-    env = envpool.make("CartPole-v1", env_type="gymnasium", num_envs=n_envs, seed=0)
-    return EnvpoolCompatibility(env)
